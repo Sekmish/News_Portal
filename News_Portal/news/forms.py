d2231ac47be14d6ec8fc110c67d392b0b85dc3d7
Aleksander <pankutak@yandex.ru>
2024-03-22 15:27:49 +0500

    Поправил forms.py

--- News_Portal/news/forms.py
+++ News_Portal/news/forms.py
@@ -14,25 +14,7 @@ class PostSearchForm(forms.ModelForm):
         ]
 
 
-    def clean(self): #используется если надо проверить несколько полей
-        cleaned_data = super().clean()
 
-        content = cleaned_data.get('content')
-        if content is not None and len(content) < 20:
-            raise ValidationError({'description': 'Описание не может быть менее 20 символов.'})
-
-        name = cleaned_data.get('name')
-        if name == content:
-            raise ValidationError('Описание не должно быть идентично названию.')
-
-        return cleaned_data
-
-
-    def clean_name(self): # Если требуется проверить одно поле сложным образом, создайте для этого метод clean_fieldname.
-        title = self.cleaned_data['title']
-        if title[0].islower():
-            raise ValidationError('Название должно начинаться с заглавной буквы.')
-        return title
 
 
 class PostForm(forms.ModelForm):
@@ -51,3 +33,22 @@ class PostForm(forms.ModelForm):
         self.fields['author'].empty_label = 'Выберите автора'
 
 
+    def clean(self): #используется если надо проверить несколько полей
+        cleaned_data = super().clean()
+
+        content = cleaned_data.get('content')
+        if content is not None and len(content) < 20:
+            raise ValidationError({'content': 'Описание не может быть менее 20 символов.'})
+
+        name = cleaned_data.get('title')
+        if name == content:
+            raise ValidationError('Описание не должно быть идентично названию.')
+
+        return cleaned_data
+
+
+    def clean_name(self): # Если требуется проверить одно поле сложным образом, создайте для этого метод clean_fieldname.
+        title = self.cleaned_data['title']
+        if title[0].islower():
+            raise ValidationError('Название должно начинаться с заглавной буквы.')
+        return title
